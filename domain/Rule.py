5c613a185f7fd9c978ad89654082ed484682e40c
lucasSimonelli <lucasp.simonelli@gmail.com>
2015-09-22 17:53:47 -0300

    Added names to rules

--- domain/Rule.py
+++ domain/Rule.py
@@ -5,12 +5,15 @@ class Rule:
     def __init__(self):
         self.conditions = []
         self.consequence = ""
+        self.name = ""
 
     @staticmethod
     # Format: cause_1,cause_2, ... ,cause_n:consequence
     def rule_from_string(raw_rule):
         rule = Rule()
-        conditions_consequence = raw_rule.split(":")
+        rule_name_and_content = raw_rule.split("-")
+        rule.name = rule_name_and_content[0]
+        conditions_consequence = rule_name_and_content[1].split(":")
         conditions = conditions_consequence[0]
         rule.consequence = conditions_consequence[1]
         rule.conditions = conditions.split(",")
--- test/ruleTests.py
+++ test/ruleTests.py
@@ -4,17 +4,19 @@ __author__ = 'Lucas'
 
 
 def test_parse_rule_one_cond():
-    rule = Rule.rule_from_string("asd:a")
+    rule = Rule.rule_from_string("R1-asd:a")
     assert rule.conditions[0] == "asd"
     assert rule.consequence == "a"
+    assert rule.name == "R1"
 
 
 def test_parse_rule_several_conds():
-    rule = Rule.rule_from_string("asd,dsa,123:b")
+    rule = Rule.rule_from_string("R2-asd,dsa,123:b")
     assert rule.conditions[0] == "asd"
     assert rule.conditions[1] == "dsa"
     assert rule.conditions[2] == "123"
     assert rule.consequence == "b"
+    assert rule.name == "R2"
 
 
 test_parse_rule_one_cond()
